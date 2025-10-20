2914e0cc4ad8c0eda40f1c70e0c9655d7bd6c821
George <george.bobovsky@gmail.com>
2025-10-21 02:09:47 +0500

    application structure

---
+++ src/components/app/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "../layout/Layout";
+import RestaurantsPageRedirect from "../../redirects/RestaurantsPageRedirect";
+import RestaurantsPage from "../../pages/restaurants/RestaurantsPage";
+import MenuPage from "../../pages/menu/MenuPage";
+import ReviewsPage from "../../pages/reviews/ReviewsPage";
+import DishPageRedirect from "../../redirects/DishPageRedirect";
+import DishPage from "../../pages/dish/DishPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="restaurants" replace />} />
+          <Route path="restaurants" element={<RestaurantsPageRedirect />}>
+            <Route path=":restaurantId" element={<RestaurantsPage />}>
+              <Route index element={<Navigate to="menu" replace />} />
+              <Route path="menu" element={<MenuPage />} />
+              <Route path="reviews" element={<ReviewsPage />} />
+              <Route path="*" element={<Navigate to="menu" replace />} />
+            </Route>
+          </Route>
+          <Route path="dish" element={<DishPageRedirect />}>
+            <Route path=":dishId" element={<DishPage />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
