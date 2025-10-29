b03c45ff69a016faf01cd9f872a942c3c9e09579
George <rockebow@gmail.com>
2025-10-29 16:41:39 +0500

    lesson 5

---
+++ src/components/restaurants/Restaurants.jsx
@@ -0,0 +1,9 @@
+import styles from "./Restaurants.module.css";
+
+export default function Restaurants() {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>Restaurants</h2>
+    </section>
+  );
+}
--- src/pages/restaurants/RestaurantsPage.jsx
+++ src/pages/restaurants/RestaurantsPage.jsx
@@ -1,1 +1,5 @@
-export default function RestaurantsPage() {}
+import Restaurants from "../../components/restaurants/Restaurants";
+
+export default function RestaurantsPage() {
+  return <Restaurants />;
+}
