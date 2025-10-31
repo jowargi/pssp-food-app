8269ca249ba8bb8c814d752a3edadca2f41092f6
pp <pp@example.com>
2025-10-31 16:42:57 +0500

    pp

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,20 @@
+import { useThemeColorContext } from "../themeColorContextProvider/ThemeColorContextProvider";
 import ThemeColorToggle from "../themeColorToggle/ThemeColorToggle";
 import styles from "./Header.module.css";
 import classNames from "classnames";
 
 export default function Header() {
+  const { themeColor } = useThemeColorContext();
+
   return (
-    <header className={classNames(styles.header, "clearfix")}>
+    <header
+      className={classNames(styles.header, styles[`header--${themeColor}`])}
+    >
       <h1
         onPointerDown={(event) => event.preventDefault()}
-        className={styles.title}
+        className={classNames(styles.title, styles[`title--${themeColor}`])}
       >
-        Food App
+        ПузоРадость
       </h1>
       <div className={styles.container}>
         <ThemeColorToggle />
--- src/components/inRestaurantNavigation/InRestaurantNavigation.jsx
+++ src/components/inRestaurantNavigation/InRestaurantNavigation.jsx
@@ -1,15 +1,24 @@
+import classNames from "classnames";
 import RouterLink from "../routerLink/RouterLink";
+import { useThemeColorContext } from "../themeColorContextProvider/ThemeColorContextProvider";
 import styles from "./InRestaurantNavigation.module.css";
 
 export default function InRestaurantNavigation() {
+  const { themeColor } = useThemeColorContext();
+
   return (
     <nav className={styles.nav}>
-      <ul className={styles.list}>
+      <ul
+        className={classNames(styles.list, {
+          [styles["list--light"]]: themeColor === "light",
+          [styles["list--dark"]]: themeColor === "dark",
+        })}
+      >
         <li>
-          <RouterLink to="menu">Menu</RouterLink>
+          <RouterLink to="menu">Меню</RouterLink>
         </li>
         <li>
-          <RouterLink to="reviews">Reviews</RouterLink>
+          <RouterLink to="reviews">Отзывы</RouterLink>
         </li>
       </ul>
     </nav>
